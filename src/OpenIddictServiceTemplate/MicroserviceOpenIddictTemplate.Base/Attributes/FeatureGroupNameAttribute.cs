c4cbf53cb0b62ca9c3f3330ee09afed4a4db9884
pepegov <andelismore@gmail.com>
2023-04-26 15:18:26 +0300

    delete base lib

--- src/OpenIddictServiceTemplate/MicroserviceOpenIddictTemplate.Base/Attributes/FeatureGroupNameAttribute.cs
+++
@@ -1,8 +0,0 @@
-namespace MicroserviceOpenIddictTemplate.Identity.Base.Attributes;
-
-[AttributeUsage(AttributeTargets.Method)]
-public class FeatureGroupNameAttribute : Attribute
-{
-    public FeatureGroupNameAttribute(string groupName) => GroupName = groupName;
-    public string GroupName { get; }
-}
--- src/OpenIddictServiceTemplate/MicroserviceOpenIddictTemplate.Base/Reposytory/IRepositoryFactory.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace MicroserviceOpenIddictTemplate.Identity.Base.Reposytory
-{
-    public interface IRepositoryFactory
-    {
-        IRepository<TEntity> GetRepository<TEntity>(bool hasCustomRepository = false) where TEntity : class;
-    }
-}
