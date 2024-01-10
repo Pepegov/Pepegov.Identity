a45553469933d4a104f86d6d2a58f75c5f6b7c85
pepegov <andelismore@gmail.com>
2024-01-10 07:26:09 +0300

    update Program.cs

--- src/OpenIddictServiceTemplate/MicroserviceOpenIddictTemplate.PL/Program.cs
+++ src/OpenIddictServiceTemplate/MicroserviceOpenIddictTemplate.PL/Program.cs
@@ -5,58 +5,64 @@ using Pepegov.MicroserviceFramework.AspNetCore.WebApplicationDefinition;
 using Serilog;
 using Serilog.Events;
 
-try
+internal class Program
 {
-    //Configure logging
-    Log.Logger = new LoggerConfiguration()
-        .MinimumLevel.Debug()
-        .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
-        .Enrich.FromLogContext()
-        .WriteTo.Console()
-        .CreateLogger();
-            
-    //Create builder
-    var builder = WebApplication.CreateBuilder(args);
-            
-    //Host logging  
-    builder.Host.UseSerilog((context, configuration) =>
-        configuration.ReadFrom.Configuration(context.Configuration));
-        
-    //Add definitions
-    var currentAssembly = typeof(Program).Assembly;
-    await builder.AddApplicationDefinitions(currentAssembly);
-        
-    //Create web application
-    var app = builder.Build();
-            
-    //Use definitions
-    await app.UseApplicationDefinitions();
-            
-    //Use logging
-    if (app.Environment.IsDevelopment())
+    public static async Task<int> Main(string[] args)
     {
-        IdentityModelEventSource.ShowPII = true;
-    }
-    app.UseSerilogRequestLogging();
+        try
+        {
+            //Configure logging
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+                .Enrich.FromLogContext()
+                .WriteTo.Console()
+                .CreateLogger();
             
-    //Run app
-    await app.RunAsync();
+            //Create builder
+            var builder = WebApplication.CreateBuilder(args);
+            
+            //Host logging  
+            builder.Host.UseSerilog((context, configuration) =>
+                configuration.ReadFrom.Configuration(context.Configuration));
+        
+            //Add definitions
+            var currentAssembly = typeof(Program).Assembly;
+            await builder.AddApplicationDefinitions(currentAssembly);
+        
+            //Create web application
+            var app = builder.Build();
+            
+            //Use definitions
+            await app.UseApplicationDefinitions();
+            
+            //Use logging
+            if (app.Environment.IsDevelopment())
+            {
+                IdentityModelEventSource.ShowPII = true;
+            }
+            app.UseSerilogRequestLogging();
+            
+            //Run app
+            await app.RunAsync();
 
-    return 1;
-}
-catch (Exception ex)
-{
-    var type = ex.GetType().Name;
-    if (type.Equals("HostAbortedException", StringComparison.Ordinal))
-    {
-        throw;
-    }
+            return 1;
+        }
+        catch (Exception ex)
+        {
+            var type = ex.GetType().Name;
+            if (type.Equals("HostAbortedException", StringComparison.Ordinal))
+            {
+                throw;
+            }
         
-    await Console.Out.WriteLineAsync(ex.Message);
-    Log.Fatal(ex, "Unhandled exception");
+            await Console.Out.WriteLineAsync(ex.Message);
+            Log.Fatal(ex, "Unhandled exception");
+        }
+        finally
+        {
+            await Log.CloseAndFlushAsync();
+        }
+        return 0;
+    }
 }
-finally
-{
-    await Log.CloseAndFlushAsync();
-}
-return 0;
