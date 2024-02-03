d837fae3b56b6f53b7b76d729107dc4586660ab7
pepegov <andelismore@gmail.com>
2024-02-03 13:16:27 +0300

    update appsettings.json & main.yml

--- src/Service/Pepegov.Identity.PL/Program.cs
+++ src/Service/Pepegov.Identity.PL/Program.cs
@@ -29,9 +29,7 @@ internal class Program
                 .Build();
     
             Log.Logger.Information($"Connection string | {testConfiguration.GetConnectionString("DefaultConnection")}");
-            Log.Logger.Information("${{{{secrets.APPSETTINGS_RABBITMQ_PASSWORD}}}}");
-            Log.Logger.Information("$secrets.APPSETTINGS_RABBITMQ_PASSWORD}");
-            Log.Logger.Information("$APPSETTINGS_RABBITMQ_PASSWORD}");
+            Log.Logger.Information("$RABBITMQ_PASSWORD");
 
             #endregion
             
